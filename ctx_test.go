195f1d192080176151b2d4d35f454363ab368ce2
Kevin Burke <kev@inburke.com>
2016-10-12 14:38:16 -0700

    Add a Duration handler
    This lets us see how much time has elapsed since we first received the request
    and can be used to more accurately measure portions of the server processing
    time.

--- ctx_test.go
+++ ctx_test.go
@@ -4,7 +4,9 @@ package handlers
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -23,5 +25,27 @@ func TestGetRequestID(t *testing.T) {
 	}
 	if uid.String() != u.String() {
 		t.Errorf("expected %s (from context) to equal %s", uid.String(), u.String())
+	}
+}
+
+func TestGetDuration(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	d := GetDuration(req.Context())
+	if d != time.Duration(0) {
+		t.Errorf("expected Duration to be 0, got %v", d)
+	}
+	w := httptest.NewRecorder()
+	ts := testServer(false)
+	h := Duration(ts)
+	h.ServeHTTP(w, req)
+	dur, err := time.ParseDuration(w.Header().Get("X-Request-Duration"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if dur == 0 {
+		t.Errorf("got 0 duration, wanted a greater than 0 duration")
+	}
+	if dur > time.Second {
+		t.Errorf("got a duration greater than 1 second: %v", dur)
 	}
 }
