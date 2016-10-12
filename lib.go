639ff8b9b784c9a322715bbbc21309c8518173a6
Kevin Burke <kev@inburke.com>
2016-10-12 15:31:44 -0700

    0.14

--- lib.go
+++ lib.go
@@ -18,7 +18,7 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-const version = "0.13"
+const version = "0.14"
 
 // All wraps h with every handler in this file.
 func All(h http.Handler, serverName string) http.Handler {
