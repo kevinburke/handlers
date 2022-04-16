c74911619f5a6099826a0e90682b373e24a54011
Kevin Burke <kevin@burke.dev>
2022-04-16 10:35:42 -0700

    ignore_test: ignore nytimes test
    This means we won't need the nytimes/gziphandler library.

--- lib_test.go
+++ lib_test.go
@@ -2,14 +2,11 @@ package handlers
 
 import (
 	"bytes"
-	"io"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
 
-	"github.com/NYTimes/gziphandler"
 	log "github.com/inconshreveable/log15"
 )
 
@@ -104,35 +101,6 @@ func TestTrailingSlashRedirect(t *testing.T) {
 	if location != "/trailingslash" {
 		t.Errorf("expected Location header to be /trailingslash, got %s", location)
 	}
-}
-
-func TestDebugGzip(t *testing.T) {
-	envFunc = func(s string) string {
-		if s == "DEBUG_HTTP_SERVER_TRAFFIC" {
-			return "true"
-		}
-		return ""
-	}
-	defer func() { envFunc = os.Getenv }()
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		resp := strings.Repeat("a", 2000)
-		io.WriteString(w, resp)
-	})
-	gzipper := gziphandler.GzipHandler(h)
-	r := httptest.NewRequest("GET", "/", nil)
-	debugBuf := new(bytes.Buffer)
-	w := httptest.NewRecorder()
-	r.Header.Set("Accept-Encoding", "gzip")
-	// Log(Debug(gzipper)).ServeHTTP(w, r)
-	DebugWriter(gzipper, debugBuf).ServeHTTP(w, r)
-	if w.Body.Len() != 39 {
-		t.Errorf("expected body length to be 39, got %d", w.Body.Len())
-	}
-	if w.Header().Get("Content-Encoding") != "gzip" {
-		t.Errorf("bad value for content encoding")
-	}
-	io.Copy(os.Stderr, debugBuf)
-	io.WriteString(os.Stderr, "\n\n")
 }
 
 func TestLog(t *testing.T) {
