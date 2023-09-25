818e9b107ec1bd7550b39aaf606af98fe3135f7a
Kevin Burke <kevin@burke.dev>
2023-09-24 21:02:54 -0700

    0.45

--- lib.go
+++ lib.go
@@ -32,7 +32,7 @@ import (
 	"github.com/kevinburke/rest/resterror"
 )
 
-const Version = "0.44"
+const Version = "0.45"
 
 func push(w http.ResponseWriter, target string, opts *http.PushOptions) error {
 	if pusher, ok := w.(http.Pusher); ok {
